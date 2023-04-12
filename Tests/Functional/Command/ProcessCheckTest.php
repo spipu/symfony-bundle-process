242fef9ade8ae4649c3ac22155df484a0876749f
Laurent Minguet <laurent@spareka.com>
2023-04-12 11:51:52 +0200

    add some unit tests

---
+++ Tests/Functional/Command/ProcessCheckTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * This file is part of a Spipu Bundle
+ *
+ * (c) Laurent Minguet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Spipu\ProcessBundle\Tests\Functional\Command;
+
+use DateTime;
+use Spipu\CoreBundle\Tests\WebTestCase;
+use Spipu\ProcessBundle\Command\ProcessCheckCommand;
+use Spipu\ProcessBundle\Exception\ProcessException;
+use Spipu\ProcessBundle\Service\ProcessManager;
+
+class ProcessCheckTest extends WebTestCase
+{
+    public function testExecuteWithoutStatus()
+    {
+        $commandTester = self::loadCommand(ProcessCheckCommand::class, 'spipu:process:check');
+        $commandTester->execute([]);
+        $this->assertSame("Number of tasks\n0", trim($commandTester->getDisplay()));
+    }
+
+    public function testExecuteWithGoodStatus()
+    {
+        $commandTester = self::loadCommand(ProcessCheckCommand::class, 'spipu:process:check');
+        $commandTester->execute(['--status' => 'created']);
+        $this->assertSame("Number of tasks in status [created]\n0", trim($commandTester->getDisplay()));
+    }
+
+    public function testExecuteWithBadStatus()
+    {
+        $this->expectException(ProcessException::class);
+        $this->expectExceptionMessage('Unknown status. Use one of created,running,finished,failed');
+
+        $commandTester = self::loadCommand(ProcessCheckCommand::class, 'spipu:process:check');
+        $commandTester->execute(['--status' => 'foo']);
+    }
+
+    public function testExecuteDirectStatus()
+    {
+        $commandTester = self::loadCommand(ProcessCheckCommand::class, 'spipu:process:check');
+        $commandTester->execute(['--status' => 'created', '--direct' => 1]);
+        $this->assertSame('0', trim($commandTester->getDisplay()));
+    }
+
+    public function testExecuteGoodCount()
+    {
+        $manager = self::getContainer()->get(ProcessManager::class);
+        $commandTester = self::loadCommand(ProcessCheckCommand::class, 'spipu:process:check');
+
+        $commandTester->execute(['--direct' => 1]);
+        $this->assertSame('0', trim($commandTester->getDisplay()));
+
+        $commandTester->execute(['--status' => 'created', '--direct' => 1]);
+        $this->assertSame('0', trim($commandTester->getDisplay()));
+
+        $commandTester->execute(['--status' => 'finished', '--direct' => 1]);
+        $this->assertSame('0', trim($commandTester->getDisplay()));
+
+        $process = $manager->load('test_sleep');
+        $manager->scheduleExecution($process, new DateTime());
+
+        $commandTester->execute(['--direct' => 1]);
+        $this->assertSame('1', trim($commandTester->getDisplay()));
+
+        $commandTester->execute(['--status' => 'created', '--direct' => 1]);
+        $this->assertSame('1', trim($commandTester->getDisplay()));
+
+        $commandTester->execute(['--status' => 'finished', '--direct' => 1]);
+        $this->assertSame('0', trim($commandTester->getDisplay()));
+    }
+}
